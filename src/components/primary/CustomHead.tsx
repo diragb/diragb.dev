f4caf84f738ce9639f71ac0f1b1cd65e458a4c7d
Dirag Biswas <hey@diragb.dev>
2024-12-14 03:50:49 +0530

    feat: seo images

--- src/components/primary/CustomHead.tsx
+++ src/components/primary/CustomHead.tsx
@@ -19,17 +19,17 @@ const CustomHead = ({
     <link rel='preconnect' href='https://fonts.googleapis.com' />
     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
     <meta name='description' content={ description ?? 'Hi, I\'m Dirag. I solve problems.' } />
-    <meta name='image' content='https://diragb.dev/square-cover.jpg' />
+    <meta name='image' content='https://diragb.dev/square-cover.png' />
     <meta property='og:url' content='https://diragb.dev' />
     <meta property='og:type' content='article' />
     <meta property='og:title' content={ title ?? 'Dirag Biswas' } />
     <meta property='og:description' content={ description ?? 'Hi, I\'m Dirag. I solve problems.' } />
-    <meta property='og:image' content='https://diragb.dev/square-cover.jpg' />
+    <meta property='og:image' content='https://diragb.dev/square-cover.png' />
     <meta name='twitter:card' content='summary_large_image' />
     <meta name='twitter:creator' content='themandirproject.com' />
     <meta name='twitter:title' content={ title ?? 'Dirag Biswas' } />
     <meta name='twitter:description' content={ description ?? 'Hi, I\'m Dirag. I solve problems.' } />
-    <meta name='twitter:image' content='https://diragb.dev/rect-cover.jpg' />
+    <meta name='twitter:image' content='https://diragb.dev/rect-cover.png' />
     <meta name='twitter:image:alt' content={ title ?? 'Dirag Biswas' } />
     <link rel='preconnect' href='https://fonts.googleapis.com' />
     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
